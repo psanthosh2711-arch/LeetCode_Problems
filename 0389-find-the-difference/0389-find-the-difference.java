91fff48525295b53334ef939127419d0e02202a3
psanthosh2711-arch <p.santhosh2711@gmail.com>
2026-07-14 00:56:37 +0530

    Time: 73 ms (6.64%), Space: 47.2 MB (8.54%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -3,29 +3,15 @@ class Solution {
         int ans=0;
         int n=s.length();
         int m=s.length();
-        if(n>m){
-            for(int i=0;i<n;i++){
-                if(i<m){
-                    ans=ans^s.charAt(i)^t.charAt(i);
-                    System.out.printf("%d",ans);
-                }
-                else{
-                    ans=ans^s.charAt(i);
-                }
+        for(int i=0;i<=m;i++){
+            if(i<n){
+                ans=ans^s.charAt(i)^t.charAt(i);
+                System.out.printf("%d",ans);
             }
-        }
-        else{
-            for(int i=0;i<=m;i++){
-                if(i<n){
-                    ans=ans^s.charAt(i)^t.charAt(i);
-                    System.out.printf("%d",ans);
-                }
-                else{
-                    ans=ans^t.charAt(i);
-                    System.out.printf("%d",ans);
-                }
+            else{
+                ans=ans^t.charAt(i);
+                System.out.printf("%d",ans);
             }
-
         }
         return (char)ans;
     }

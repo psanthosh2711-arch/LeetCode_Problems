12030ac64230e6df969938972f0e657e006c0cef
psanthosh2711-arch <p.santhosh2711@gmail.com>
2026-08-17 23:31:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (65.7%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,33 +1,19 @@
 class Solution {
     public boolean check(int[] nums) {
-        int i=0;
-        while(i<nums.length-1){
-            if(nums[i]>nums[i+1]){
-                int pivot=nums[i+1];
-                int j=i+1;
-                while(j<nums.length){
-                    if(nums[j]>=pivot && nums[j]<=nums[0]){
-                        pivot=nums[j];
-                        j=j+1;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-                if(j==nums.length){
-                    return true;
-                }
-            }
-            else{
-                i=i+1;
-            }
+       int count=0;
+       for(int i=1;i<nums.length;i++){
+        if(nums[i]<nums[i-1]){
+            count=count+1;
         }
-        if(i==nums.length-1){
-            return true;
-        }
-        else{
-            return false;
-        }
-
+       }
+       if(nums[nums.length-1]>nums[0]){
+        count=count+1;
+       }
+       if(count>1){
+        return false;
+       }
+       else{
+        return true;
+       }
     }
 }
